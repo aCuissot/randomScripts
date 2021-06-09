0b3849c3bd9fbfa085aa70e8041bb16177d0af4e
aCuissot <harrypotter9752@gmail.com>
2021-06-09 22:40:12 +0200

    update script

--- Roland Garros/RolandGarros.md
+++ Roland Garros/RolandGarros.md
@@ -4,6 +4,6 @@ To use it, you can type the google request "roland garros" in your browser and i
 
 Then you can get the right URL in your browser console. To open the console, press `Ctrl`+`CAPS`+`c` and got in the 'Network' tab.
 Double click as show bellow, it will open a new window to download a text file. You don't have to download it, but copy the URL of the page and thanks to it, you will be able to see the match live score in your terminal
-![How to get the link](getLink.png) 
+![How to get the link](media/getLink.png) 
 In some browser, it will download automatically the .txt file, so to get the url, you can try to copy the link differently, like for me in edge:
-![How to get the link with edge](Edge.png) 
+![How to get the link with edge](media/Edge.png) 
--- Roland Garros/rolandGarros.py
+++ Roland Garros/rolandGarros.py
@@ -1,4 +1,8 @@
+import os
+import time
+
 import requests
+import platform
 
 
 def clearLine(line, haveSpace=False):
@@ -7,19 +11,18 @@ def clearLine(line, haveSpace=False):
     return line.split('"')[1]
 
 
-url = "https://www.google.com/async/torspo?ei=qtXAYIvUMKmSjLsPkP2puAo&yv=3&async=emids:%2Fg%2F11rfwjq3qm,id:lu,ctx:%5B%5B%5B3%2C%22%2Fm%2F012xcl%22%5D%0A%2C%5B2%5D%0A%2C%5B1%5D%0A%5D%0A%2Cnull%2C%5B%5B%22ev2%22%2C%22clb%22%2C%22tlb%22%2C%22tv%22%2C%22gs2%22%2C%22msv%22%5D%0A%5D%0A%2Cnull%2C1%2Cnull%2C0%2C%5Bnull%2C%5B%22%2Fg%2F11ljs58syp%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F07bs0%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F012xcl%22%5D%0A%5D%0A%2C2%2C0%5D%0A,dme:,ct:FR,hl:fr,tz:Europe%2FParis,_fmt:jspb"
-url = "https://www.google.fr/async/torspo?ei=LQTBYPqdN5mVjLsPg4aSyA4&yv=3&async=emids:%2Fg%2F11nn797dgr,id:lu,ctx:%5B%5B%5B3%2C%22%2Fm%2F012xcl%22%5D%0A%2C%5B2%5D%0A%2C%5B1%5D%0A%5D%0A%2Cnull%2C%5B%5B%22ev2%22%2C%22clb%22%2C%22tlb%22%2C%22tv%22%2C%22gs2%22%2C%22msv%22%5D%0A%5D%0A%2Cnull%2C1%2Cnull%2C0%2C%5Bnull%2C%5B%22%2Fg%2F11ljs58syp%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F07bs0%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F012xcl%22%5D%0A%5D%0A%2C2%2C0%5D%0A,dme:,ct:FR,hl:en,tz:Europe%2FParis,_fmt:jspb"
 
-r = requests.get(url, allow_redirects=True)
-
-content = r.content
-content = content.decode("utf-8")
-
-contentLines = content.splitlines()
 
 """
 assuming that lines will always contain the same data -> C fo
 """
+
+
+def clear_console(curr_os):
+    if curr_os=='Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 
 def getPlayers(txtArray):
@@ -113,18 +116,56 @@ def getStats(txtArray):
     return res
 
 
-print(getMatch(contentLines))
-print(getCourt(contentLines))
+def getStatus(txtArray):
+    statusTag = '["tsp-lv","tsp-fm"]'
+    i = 0
+    for element in txtArray:
+        i += 1
+        if statusTag in element:
+            return clearLine(txtArray[i])
 
-print(getPlayers(contentLines))
-print("Score:")
-j1Sets, j2Sets = getSets(contentLines)
-print(j1Sets)
-print(j2Sets)
 
-print(getJeux(contentLines))
+def displayResult(curr_os, refreshRate=30, displayStats=False):
+    """
 
-print("===========")
-print("Stats:")
-for stat in getStats(contentLines):
-    print(stat)
+    :param curr_os: current OS to be able to clear and so refresh console
+    :param refreshRate: In seconds. Stats should not be updated with a very high frequency, so it is useless refresh it to frequently
+    :param displayStats: Optional: True if you want to display stats too, False if you dont. Default: False
+    """
+    isInLive=True
+    while(isInLive):
+        r = requests.get(url, allow_redirects=True)
+
+        content = r.content
+        content = content.decode("utf-8")
+
+        contentLines = content.splitlines()
+        clear_console(curr_os)
+
+        print(getMatch(contentLines))
+        print(getCourt(contentLines))
+
+        print(getPlayers(contentLines))
+        print("Score:")
+        j1Sets, j2Sets = getSets(contentLines)
+        print(j1Sets)
+        print(j2Sets)
+
+        print(getJeux(contentLines))
+        if displayStats:
+            print("===========")
+            print("Stats:")
+            for stat in getStats(contentLines):
+                print(stat)
+        if not('direct' in getStatus(contentLines).lower() or 'live' in getStatus(contentLines).lower()):
+            isInLive=False
+        time.sleep(refreshRate)
+
+
+url = "https://www.google.com/async/torspo?ei=qtXAYIvUMKmSjLsPkP2puAo&yv=3&async=emids:%2Fg%2F11rfwjq3qm,id:lu,ctx:%5B%5B%5B3%2C%22%2Fm%2F012xcl%22%5D%0A%2C%5B2%5D%0A%2C%5B1%5D%0A%5D%0A%2Cnull%2C%5B%5B%22ev2%22%2C%22clb%22%2C%22tlb%22%2C%22tv%22%2C%22gs2%22%2C%22msv%22%5D%0A%5D%0A%2Cnull%2C1%2Cnull%2C0%2C%5Bnull%2C%5B%22%2Fg%2F11ljs58syp%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F07bs0%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F012xcl%22%5D%0A%5D%0A%2C2%2C0%5D%0A,dme:,ct:FR,hl:fr,tz:Europe%2FParis,_fmt:jspb"
+url = "https://www.google.com/async/torspo?ei=LQTBYPqdN5mVjLsPg4aSyA4&yv=3&async=emids:%2Fg%2F11nn797dgr,id:lu,ctx:%5B%5B%5B3%2C%22%2Fm%2F012xcl%22%5D%0A%2C%5B2%5D%0A%2C%5B1%5D%0A%5D%0A%2Cnull%2C%5B%5B%22ev2%22%2C%22clb%22%2C%22tlb%22%2C%22tv%22%2C%22gs2%22%2C%22msv%22%5D%0A%5D%0A%2Cnull%2C1%2Cnull%2C0%2C%5Bnull%2C%5B%22%2Fg%2F11ljs58syp%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F07bs0%22%5D%0A%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%22%2Fm%2F012xcl%22%5D%0A%5D%0A%2C2%2C0%5D%0A,dme:,ct:FR,hl:en,tz:Europe%2FParis,_fmt:jspb"
+
+
+
+currOs = platform.system()
+displayResult(currOs, refreshRate=15)
